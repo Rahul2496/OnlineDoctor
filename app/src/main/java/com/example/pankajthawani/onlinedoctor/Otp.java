906df3070125174e2be0871cd9dd4d3213bd2591
thawani.pankaj7 <thawani.pankaj7@gmail.com>
2018-03-19 23:41:53 +0530

    otp

--- app/src/main/java/com/example/pankajthawani/onlinedoctor/Otp.java
+++ app/src/main/java/com/example/pankajthawani/onlinedoctor/Otp.java
@@ -1,17 +1,24 @@
 package com.example.pankajthawani.onlinedoctor;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class Otp extends AppCompatActivity {
-    TextView otp;
+    TextView otp,help;
     Button submit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_otp);
-
+        help=findViewById(R.id.helpon);
+    }
+    public void on_help(View view)
+    {
+        //Intent intent=new Intent(Otp.this,Help.class)
+        //startActivity(intent);
     }
 }
