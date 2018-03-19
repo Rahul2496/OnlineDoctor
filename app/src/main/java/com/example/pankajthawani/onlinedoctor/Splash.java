92bb8c5212c902601831f77a47c52b6338b325c2
thawani.pankaj7 <thawani.pankaj7@gmail.com>
2018-03-19 20:31:49 +0530

    Initial commit Splash screen created

---
+++ app/src/main/java/com/example/pankajthawani/onlinedoctor/Splash.java
@@ -0,0 +1,28 @@
+package com.example.pankajthawani.onlinedoctor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Thread timer = new Thread(){
+            public void run(){
+                try{
+                    sleep(3500);
+                }
+                catch(InterruptedException e){
+                    e.printStackTrace();
+                } finally {
+                    //Code to intent to move this from registration activity
+                    finish();
+                }
+            }
+        };
+        timer.start();
+    }
+}
